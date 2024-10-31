8be301698119698b327e76f60e6bea2ed9ec2a5c
Leonardo <vr.leonardo2020@gmail.com>
2024-10-31 08:48:50 -0300

    configuring the project with the Entity Framework and administrators table

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+
 namespace MinimalApi.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto: DbContext
 {
-    
+    private readonly IConfiguration _ConfiguracaoAppSettings;
+    public DbContexto(IConfiguration ConfiguracaoAppSettings)
+    {
+        _ConfiguracaoAppSettings = ConfiguracaoAppSettings;
+    }
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+            var stringConexao = _ConfiguracaoAppSettings.GetConnectionString("mysql")?.ToString();
+            if(!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(
+                    stringConexao,
+                    ServerVersion.AutoDetect(stringConexao)
+                );
+            }
+        }
+        
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(
+    options => {
+        options.UseMySql(
+            builder.Configuration.GetConnectionString("mysql"),
+            ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+        );
+    }
+);
+
 var app = builder.Build();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+app.MapGet("/login", (LoginDTO loginDTO) =>
 {
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
     {
